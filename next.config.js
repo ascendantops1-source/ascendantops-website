40f1cd93c733c3ab98445d1db0d01244f80dc346
ascendantops1-source <ascendant.ops1@gmail.com>
2026-06-25 11:39:21 -0600

    Configure Next.js config for Static HTML Export on Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
+  output: 'export', // Enables Static HTML Export
+  images: {
+    unoptimized: true, // Required: Static exports do not support dynamic server-side image resizing
+  },
+  webpack: (config) => {
     config.cache = false;
     return config;
   },
